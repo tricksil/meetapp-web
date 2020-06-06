4bad334a949dc71693dc3c3d1a7c465b8cad4572
Patrick Silva <patrickeeep12@gmail.com>
2020-06-06 19:56:07 -0300

    Config reactotron, routes, layouts and global styles

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '~/assets/logo.svg';
+
+function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+    </>
+  );
+}
+
+export default SignIn;
---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-180deg, #22202c, #402845);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div``;
