1f3651a4a8914493ccdfd73eb39b10aa0bbd4d27
Patrick Silva <patrickeeep12@gmail.com>
2020-06-06 20:33:34 -0300

    Stylizing signIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('Senha é obrigatório'),
+});
+
 function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Meetapp" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          autoComplete="off"
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </Form>
     </>
   );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -9,4 +10,56 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+
+    input {
+      border: 0;
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      height: 50px;
+      padding: 0 15px;
+      font-size: 18px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      border: 0;
+      background: #f94d6a;
+      height: 50px;
+      border-radius: 4px;
+      font-weight: bold;
+      font-size: 16px;
+      color: #fff;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.06, '#f94d6a')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
