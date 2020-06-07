359415c47a63676c7f97016155525be91abe62eb
Patrick Silva <patrickeeep12@gmail.com>
2020-06-07 05:35:19 -0300

    Stylizing SIgnUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('Senha é obrigatório'),
+});
 
 function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input
+          name="name"
+          type="text"
+          autoComplete="off"
+          placeholder="Digite seu nome completo"
+        />
+        <Input
+          name="email"
+          type="email"
+          autoComplete="off"
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignUp;
