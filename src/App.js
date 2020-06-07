7f32becf8b4fedf20b9210fb53fb613c51319c93
Patrick Silva <patrickeeep12@gmail.com>
2020-06-07 07:52:55 -0300

    Connect with redux on application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import '~/config/ReactotronConfig';
 
@@ -8,12 +11,19 @@ import history from '~/services/history';
 
 import GlobalStyles from '~/styles/global';
 
+import { store, persistor } from './store';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
